8ddc503ff3ab25b26dc1025a33af4e0bdd19b982
Flo E <ehr@etud.insa-toulouse.fr>
2021-03-24 17:07:43 +0100

    test getLength() reussi

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -209,12 +209,14 @@ public class Path {
      * Compute the length of this path (in meters).
      * 
      * @return Total length of the path (in meters).
-     * 
-     * @deprecated Need to be implemented.
      */
     public float getLength() {
         // TODO:
-        return 0;
+    	float total = 0;
+    	for (Arc a : this.arcs) {
+    		total += a.getLength();
+    	}
+        return total;
     }
 
     /**
@@ -224,12 +226,10 @@ public class Path {
      * 
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
-     * 
-     * @deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
         // TODO:
-        return 0;
+        return (this.getLength()/speed);
     }
 
     /**
